038345b2dd40d4b3925588295dbb1ec7b67ed1c5
kofgoodzb <kofgoodzb@gmail.com>
2016-08-04 16:58:55 +0800

    Algorithm Test

---
+++ Bubble Sort.ps1
@@ -0,0 +1,18 @@
+﻿
+#$Number = "23,43,12,7,87,53,56,91,2,33,61"
+#$NumberArray = $Number.Split(",")
+
+$NumberArray = 1..20 | ForEach-Object {Get-Random -Maximum 1000}
+
+for ($j=0;$j -le $NumberArray.Count -1;$j++){
+  for ($i=0;$i -le $NumberArray.Count - 1;$i++)
+     {
+      if([int]$NumberArray[$i] -le [int]$NumberArray[$i+1] ) 
+          { 
+             $N = $NumberArray[$i]
+             $NumberArray[$i] = $NumberArray[$i+1]
+             $NumberArray[$i+1] = $N
+            } 
+      }
+}
+     Write-Host $NumberArray -NoNewline
---
+++ Straight Insertion Sort.ps1
@@ -0,0 +1,20 @@
+﻿$NumberArray = 1..20 | ForEach-Object {Get-Random -Maximum 1000}
+Write-Host $NumberArray -NoNewline
+Write-Host ""
+
+for ($i=0;$i -lt $NumberArray.Count; $i++ ){ 
+
+             $val = $NumberArray[$i]
+             $j = $i-1
+                while($j -ge 0 -and $NumberArray[$j] -gt $val) 
+                   {
+                       $NumberArray[$j+1] = $NumberArray[$j]
+                       $j--
+                    }
+                  $NumberArray[$j+1]=$val
+        }
+
+Write-Host  $NumberArray -NoNewline -ForegroundColor Yellow
+
+
+
